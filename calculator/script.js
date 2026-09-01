4b8e8657e194a1e5e40d492ed52af660e7dd2dd2
Priyanshu <priyanshuwork127@gmail.com>
2026-09-01 10:50:11 +0530

    event

---
+++ calculator/script.js
@@ -0,0 +1,6 @@
+console.log("JS CONNECTED")
+
+let display=document.getElementById("display");
+document.addEventListener("keyboard",function(e){
+    console.log(e.key);
+});
---
+++ javascript/student info system/studentinfo.js
@@ -0,0 +1,14 @@
+function getdata(){
+    let n=prompt("Enter the name");
+    let r=prompt("Enter the roll no");
+    let d=prompt("Enter the department");
+    let nc=document.getElementById("name");
+    let rl=document.getElementById("roll");
+    let dt=document.getElementById("dept");
+    nc.innerHTML=n;
+    rl.innerHTML=r;
+    dt.innerHTML=d;
+}
+function marks(){
+    
+}
